5c5ac6eb7040604850472e517dc9e139212614f9
LukeTo <31716165+muronaldo13@users.noreply.github.com>
2022-03-16 17:46:36 +1100

    Add getArnieQuotes

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,40 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+enum ResultName {
+  FAILURE = 'FAILURE',
+  ARNIE_QUOTE = 'Arnie Quote'
+}
+
+interface SuccessResult {
+  [ResultName.FAILURE]: string;
+  [ResultName.ARNIE_QUOTE]?: never;
+}
+interface FailureResult {
+  [ResultName.FAILURE]?: never;
+  [ResultName.ARNIE_QUOTE]: string;
+}
+
+type TResult = SuccessResult | FailureResult;
+
+const convertStringToJSON = (content: string): Record<string, any> => JSON.parse(content);
+
+const getUrlReturnResult = async (url: string): Promise<TResult> => {
+  const res = await httpGet(url);
+
+  const message: string = convertStringToJSON(res.body).message;
+
+  if (res.status !== 200) {
+    return {
+      [ResultName.FAILURE]: message,
+    }
+  }
+  return {
+    [ResultName.ARNIE_QUOTE]: message
+  }
+}
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+  const urlPromises = urls.map(getUrlReturnResult);
+
+  return Promise.all(urlPromises)
 };
